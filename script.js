14bac140d3f93e5d607b3ed07a056a971e19bda2
Pedro-Garcia-1 <pedrogarciacarvalho2114@gmail.com>
2026-02-03 11:10:04 -0300

    second section is done, making third now. Almost done too, just trying to put details in it and make a contact btn

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting){
-            console.log(entry.target);
             entry.target.classList.add('show');
         }else{
             entry.target.classList.remove('show');
@@ -12,3 +11,15 @@ const observer = new IntersectionObserver((entries) => {
 const todoElements = document.querySelectorAll('.todo');
 
 todoElements.forEach(item => observer.observe(item));
+
+// const observer2 = new IntersectionObserver((entry) => {
+//     entries.forEach((entry) => {
+//         if(entry.isIntersecting){
+//             console.log(section3);
+//         }
+//     })
+// }, {});
+
+// const section3 = document.querySelectorAll('.section-3');
+
+// observer2.forEach(item => observer.observe(section3));
